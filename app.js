13412d0a41f0181e97d9ac6c7c10047e0370db4f
anwar8983ali <158761862+anwar8983ali@users.noreply.github.com>
2025-07-12 19:12:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.getElementById("darkToggle");
-  const menuToggle = document.getElementById("menuToggle");
-  const navLinks = document.getElementById("navLinks");
 
   toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
   });
-
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-  });
 });
+
 
 
